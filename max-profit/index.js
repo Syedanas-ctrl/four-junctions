5f52210ce135eb86f47412f1b6d3e1135df0274b
Syed Anas <Faiqh2001@gmail.com>
2025-05-01 11:51:02 +0530

    fix: max profit

--- max-profit/index.js
+++ max-profit/index.js
@@ -1,4 +1,11 @@
 function maxProfit(n) {
+    if (isNaN(n)) {
+        return {
+            earnings: 0,
+            solution: 'Invalid input'
+        };
+    }
+    
     // Property definitions
     const properties = [
         { name: 'T', time: 5, earning: 1500 },
@@ -15,8 +22,12 @@ function maxProfit(n) {
         for (let i = 0; i < properties.length; i++) {
             const { name, time, earning } = properties[i];
             if (t >= time) {
-                if (dp[t] < dp[t - time] + earning) {
-                    dp[t] = dp[t - time] + earning;
+                // Calculate operational time and earnings
+                const operationalTime = t - time;  // Time building is operational
+                const totalEarning = operationalTime * earning;
+                
+                if (dp[t] < dp[t - time] + totalEarning) {
+                    dp[t] = dp[t - time] + totalEarning;
                     choice[t] = i; // store index of property chosen
                 }
             }
@@ -40,6 +51,9 @@ function maxProfit(n) {
 }
 
 // Example usage:
-console.log(maxProfit(7));   // { earnings: 3000, solution: 'T: 1 P: 0 C: 0' }
-console.log(maxProfit(8));   // { earnings: 4500, solution: 'T: 1 P: 1 C: 0' }
-console.log(maxProfit(13));  // { earnings: 16500, solution: 'T: 2 P: 2 C: 0' }
+console.log(maxProfit(0));  
+console.log(maxProfit(0/0));  
+console.log(maxProfit(7));
+console.log(maxProfit(8));  
+console.log(maxProfit(13)); 
+console.log(maxProfit(49)); 
